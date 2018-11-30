83d0811a40898ce76656faf2a610f735ca8a15f6
NelsonChou <42751275+NelsonChou@users.noreply.github.com>
2018-11-30 15:34:17 -0500

    Update Unstructured Data Final Project.py

--- Unstructured Data Final Project.py
+++ Unstructured Data Final Project.py
@@ -1,6 +1,4 @@
 ####Unstructured Data Final Project
-
-
 import pandas as pd
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
@@ -13,6 +11,23 @@ df_raw=pd.read_excel('Sheet for classification.xlsx')
 #Merge title and text columns
 df_raw['Classification']=df_raw['Classification'].str.lower()
 df_raw['title_text_combined']=df_raw['Title'].map(str)+df_raw['Text'].map(str)
+
+
+df_raw['Classification_numeric']=0
+for i in range(len(df_raw['Classification'])):
+    if df_raw['Classification'][i]=='bracelet':
+        df_raw['Classification_numeric'][i]=0
+    elif df_raw['Classification'][i]=='earring':
+        df_raw['Classification_numeric'][i]=1
+    elif df_raw['Classification'][i]=='necklace':
+        df_raw['Classification_numeric'][i]=2
+    elif df_raw['Classification'][i]=='ring':
+        df_raw['Classification_numeric'][i]=3
+    elif df_raw['Classification'][i]=='watch':
+        df_raw['Classification_numeric'][i]=4
+    elif df_raw['Classification'][i]=='other':
+        df_raw['Classification_numeric'][i]=5
+    else: 6
 
 #Tokenization
 full_string_list=[]
@@ -51,45 +66,60 @@ df_for_model=pd.DataFrame(array_final)
 
 #Split into train and test set
 
-training_x=df_for_model[0:340]
-training_c=df_raw[0:340]['Classification']
+training_x=df_for_model[0:390]
+training_c=df_raw[0:390]['Classification_numeric']
 
-testing_x=df_for_model[341:]
-testing_c=df_raw[341:]['Classification']
+testing_x=df_for_model[391:]
+testing_c=df_raw[391:]['Classification_numeric']
 
+#########
+#Modeling
+#########
+
+##############
 ## Naive Bayes
+##############
+from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import MultinomialNB
 NBmodel = MultinomialNB()
+
 # training
 NBmodel.fit(training_x, training_c)
 y_pred_NB = NBmodel.predict(testing_x)
+
 # evaluation
 acc_NB = accuracy_score(testing_c, y_pred_NB)
 print("Naive Bayes model Accuracy:: {:.2f}%".format(acc_NB*100))
 
+##############
 #SVC
+##############
 from sklearn.svm import LinearSVC
 SVMmodel = LinearSVC()
+
 # training
 SVMmodel.fit(training_x, training_c)
 y_pred_SVM = SVMmodel.predict(testing_x)
+
 # evaluation
 acc_SVM = accuracy_score(testing_c, y_pred_SVM)
-print("SVM model Accuracy:
-{:.2f}%".format(acc_SVM*100))
+print("SVM model Accuracy:{:.2f}%".format(acc_SVM*100))
 
-#Decision Tree and Random Forest      
+################################
+#Decision Tree and Random Forest
+################################      
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 DTmodel = DecisionTreeClassifier()
 RFmodel = RandomForestClassifier(n_estimators=50, max_depth=3,
-bootstrap=True, random_state=0) ## number of trees and number of
-layers/depth
+bootstrap=True, random_state=0) ## number of trees and number of layers/depth
+
 # training
 DTmodel.fit(training_x, training_c)
 y_pred_DT = DTmodel.predict(testing_x)
 RFmodel.fit(training_x, training_c)
 y_pred_RF = RFmodel.predict(testing_x)
+
 # evaluation
 acc_DT = accuracy_score(testing_c, y_pred_DT)
 print("Decision Tree Model Accuracy: {:.2f}%".format(acc_DT*100))
